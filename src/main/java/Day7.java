0fc531120457da511d3e890832810ceb3ebd16c4
ce-007 <tom735593@gmail.com>
2024-12-08 14:45:56 -0800

    Day Seven: Optimized

--- src/main/java/Day7.java
+++ src/main/java/Day7.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day7 {
     public static void main(String[] args) throws FileNotFoundException {
@@ -10,10 +11,10 @@ public class Day7 {
         long totalP2 = 0;
         while (scanner.hasNextLine()) {
             String[] line = scanner.nextLine().split(": ");
-            if (checkIfCouldBeTrue(Long.parseLong(line[0]), List.of(line[1].split(" ")), 3)) {
+            if (checkIfCouldBeTrue(Long.parseLong(line[0]), List.of(line[1].split(" ")).stream().map(Long::parseLong).collect(Collectors.toList()), 3)) {
                 totalP2 += Long.parseLong(line[0]);
             }
-            if (checkIfCouldBeTrue(Long.parseLong(line[0]), List.of(line[1].split(" ")), 2)) {
+            if (checkIfCouldBeTrue(Long.parseLong(line[0]), List.of(line[1].split(" ")).stream().map(Long::parseLong).collect(Collectors.toList()), 2)) {
                 totalP1 += Long.parseLong(line[0]);
             }
         }
@@ -21,13 +22,14 @@ public class Day7 {
         System.out.println("Part two: " + totalP2);
     }
 
-    private static boolean checkIfCouldBeTrue(long outcome, List<String> input, int power) {
+    private static boolean checkIfCouldBeTrue(long outcome, List<Long> input, int power) {
         int[] operators = new int[input.size() - 1];
         for (int i = 0; i < operators.length; i++) {
             for (int j = 0; j < Math.pow(power, operators.length); j++) {
                 for (int k = 1; k < operators.length + 1; k++) {
-                    operators[(i + j) % k]++;
-                    operators[(i + j) % k] %= power;
+                    int temp = (i + j) % k;
+                    operators[temp]++;
+                    operators[temp] %= power;
                     if (checkIfSame(input, operators) == outcome) {
                         return true;
                     }
@@ -42,15 +44,15 @@ public class Day7 {
         return false;
     }
 
-    private static long checkIfSame(List<String> input, int[] operators) {
-        long total = Long.parseLong(input.getFirst());
+    private static long checkIfSame(List<Long> input, int[] operators) {
+        long total = input.getFirst();
         for (int k = 0; k < operators.length; k++) {
-            if (operators[k] % 3 == 0) {
-                total *= Long.parseLong(input.get(k + 1));
-            } else if (operators[k] % 3 == 1) {
-                total += Long.parseLong(input.get(k + 1));
+            if (operators[k] == 0) {
+                total *= input.get(k + 1);
+            } else if (operators[k] == 1) {
+                total += input.get(k + 1);
             } else {
-                String string = total + input.get(k + 1);
+                String string = total + input.get(k + 1).toString();
                 total = Long.parseLong(string);
             }
         }
