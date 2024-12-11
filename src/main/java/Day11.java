ab7766a17ce2fa49063d317c179093d6ef0b3f7f
ce-007 <tom735593@gmail.com>
2024-12-11 14:41:23 -0800

    Day 11: Part two optimized

--- src/main/java/Day11.java
+++ src/main/java/Day11.java
@@ -1,8 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Day11 {
     public static void main(String[] args) throws FileNotFoundException {
@@ -15,23 +13,96 @@ public class Day11 {
         }
 
         scanner.close();
-        for (int i = 0; i < 75; i++) {
-            for (int j = 0; j < input.size(); j++) {
-                if (input.get(j) == 0L) {
-                    input.set(j, 1L);
-                } else if (input.get(j).toString().length() % 2 == 0) {
-                    int size = input.get(j).toString().length();
-                    String temp = input.get(j).toString();
-                    input.set(j, Long.parseLong(temp.substring(0, size / 2)));
-                    input.add(j + 1, Long.parseLong(temp.substring(size / 2)));
+        Map<Long, List<Long>> calculated = new HashMap<>();
+        LinkedHashMap<Long, Long> amountOfEach = new LinkedHashMap<>();
+        List<Long> first = new ArrayList<>();
+        first.add(1L);
+        calculated.put(0L, first);
 
-                    j++;
+        for (int i = 0; i < input.size(); i++) {
+            amountOfEach.put(input.get(i), 1L);
+        }
+        System.out.println("Part One: " + calculate(amountOfEach, calculated, 25));
+        System.out.println("Part Two: " + calculate(amountOfEach, calculated, 50));
+    }
+
+    private static long calculate(LinkedHashMap<Long, Long> input, Map<Long, List<Long>> calculated, int part) {
+        for (int i = 0; i < part; i++) {
+            Map<Long, Long> keysToModify = new HashMap<>();
+            List<Map.Entry<Long, List<Long>>> calculatedEntries = new ArrayList<>();
+            Map<Long, Long> keysToDecrement = new HashMap<>();
+
+            for (Map.Entry<Long, Long> entry : input.entrySet()) {
+                if (calculated.containsKey(entry.getKey())) {
+                    List<Long> temp = calculated.get(entry.getKey());
+
+                    if (keysToModify.containsKey(temp.get(0))) {
+                        keysToModify.put(temp.getFirst(), keysToModify.get(temp.getFirst()) + entry.getValue());
+                    } else {
+                        keysToModify.put(temp.getFirst(), entry.getValue());
+                    }
+                    keysToDecrement.put(entry.getKey(), entry.getValue());
+                    if (temp.size() > 1) {
+                        if (keysToModify.containsKey(temp.get(1))) {
+                            keysToModify.put(temp.get(1), keysToModify.get(temp.get(1)) + entry.getValue());
+                        } else {
+                            keysToModify.put(temp.get(1), entry.getValue());
+                        }
+                    }
+
+                } else if (entry.toString().length() % 2 == 0) {
+                    int size = entry.getKey().toString().length();
+                    String temp = entry.getKey().toString();
+                    Long a = Long.parseLong(temp.substring(0, size / 2));
+                    Long b = Long.parseLong(temp.substring(size / 2));
+                    List<Long> tempList = new ArrayList<>();
+                    tempList.add(a);
+                    tempList.add(b);
+                    calculatedEntries.add(Map.entry(entry.getKey(), tempList));
+                    keysToDecrement.put(entry.getKey(), entry.getValue());
+
+                    if (keysToModify.containsKey(a)) {
+                        keysToModify.put(a, keysToModify.get(a) + entry.getValue());
+                    } else {
+                        keysToModify.put(a, entry.getValue());
+                    }
+                    if (keysToModify.containsKey(b)) {
+                        keysToModify.put(b, keysToModify.get(b) + entry.getValue());
+                    } else {
+                        keysToModify.put(b, entry.getValue());
+                    }
                 } else {
-                    input.set(j, input.get(j) * 2024);
+                    List<Long> temp = new ArrayList<>();
+                    temp.add(entry.getKey() * 2024L);
+                    keysToDecrement.put(entry.getKey(), entry.getValue());
+                    calculatedEntries.add(Map.entry(entry.getKey(), temp));
+
+                    if (keysToModify.containsKey(temp.getFirst())) {
+                        keysToModify.put(temp.getFirst(), keysToModify.get(temp.getFirst()) + entry.getValue());
+                    } else {
+                        keysToModify.put(temp.getFirst(), entry.getValue());
+                    }
+
                 }
             }
-        }
 
-        System.out.println(input.size());
+            for (Long key : keysToDecrement.keySet()) {
+                if (input.containsKey(key)) {
+                    input.remove(key, keysToDecrement.get(key));
+                }
+            }
+            for (Long key : keysToModify.keySet()) {
+                input.putIfAbsent(key, 0L);
+                input.replace(key, keysToModify.get(key));
+            }
+            for (Map.Entry<Long, List<Long>> entry : calculatedEntries) {
+                calculated.put(entry.getKey(), entry.getValue());
+            }
+        }
+        Long out = 0L;
+        for (Map.Entry<Long, Long> entry : input.entrySet()) {
+            out += entry.getValue();
+        }
+        return out;
     }
 }
