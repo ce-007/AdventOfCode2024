f0056b88e61cce85cd79656615d2af2bc841b692
ce-007 <tom735593@gmail.com>
2024-12-16 08:34:19 -0800

    Day 13: Part two

--- src/main/java/Day13.java
+++ src/main/java/Day13.java
@@ -1,6 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.Scanner;
 
 public class Day13 {
     public static void main(String[] args) throws FileNotFoundException {
@@ -9,37 +9,34 @@ public class Day13 {
         long tokens = 0;
         while (scanner.hasNextLine()) {
             String a = scanner.nextLine();
-            int aX = Integer.parseInt(a.substring(12, 14));
-            int aY = Integer.parseInt(a.substring(18));
+            long aX = Integer.parseInt(a.substring(12, 14));
+            long aY = Integer.parseInt(a.substring(18));
             String b = scanner.nextLine();
-            int bX = Integer.parseInt(b.substring(12, 14));
-            int bY = Integer.parseInt(b.substring(18));
+            long bX = Integer.parseInt(b.substring(12, 14));
+            long bY = Integer.parseInt(b.substring(18));
             String p = scanner.nextLine();
-            int pX = Integer.parseInt(p.substring(9, p.indexOf(',')));
-            int pY = Integer.parseInt(p.substring(p.lastIndexOf('=') + 1));
+            long pX = Integer.parseInt(p.substring(9, p.indexOf(','))) + 10000000000000L;
+            long pY = Integer.parseInt(p.substring(p.lastIndexOf('=') + 1)) + 10000000000000L;
             scanner.nextLine();
-            for (int i = 0; i < pX / aX + 1; i++) {
-                int x = 0;
-                int y = 0;
-                int c = 0;
-                int d = 0;
-                for (int j = 0; x < pX; j++) {
-                    if (j < i) {
-                        x += aX;
-                        y += aY;
-                        d++;
-                    } else {
-                        x += bX;
-                        y += bY;
-                        c++;
-                    }
-                }
-                //System.out.println(x + " " + pX + " " + y + " " + pY);
-                if (x == pX && y == pY) {
-                    //System.out.println(c + d * 3);
-                    tokens += c + d * 3;
-                    i = pX / aX;
-                }
+
+            long delta = aX * bY - aY * bX;
+
+            if (delta == 0) {
+                continue;
+            }
+
+            long deltaI = pX * bY - pY * bX;
+            long deltaJ = aX * pY - aY * pX;
+
+            if (deltaI % delta != 0 || deltaJ % delta != 0) {
+                continue;
+            }
+
+            long i = deltaI / delta;
+            long j = deltaJ / delta;
+
+            if (i >= 0 && j >= 0) {
+                tokens += j + i * 3;
             }
         }
         System.out.println(tokens);
